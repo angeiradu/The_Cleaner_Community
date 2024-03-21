420a14928f44dbc064f8cecfe98cdf0be975dad8
angeiradu <a.iradukund2@alustudent.com>
2024-03-21 07:01:34 -0700

    responsive for sidebar and dashboard

---
+++ src/Data/DashboardData.js
@@ -0,0 +1,32 @@
+import { FaUserSlash } from 'react-icons/fa';
+import { PiStudentDuotone } from 'react-icons/pi';
+import { GiTeacher } from 'react-icons/gi';
+import { FiUsers } from 'react-icons/fi';
+import { FaUserSecret } from 'react-icons/fa';
+export const DashboardData = [
+    {
+        icons:<PiStudentDuotone />,
+        name: 'Users',
+        number: '323',
+    },
+    {
+        icons:<GiTeacher />,
+        name: 'Active Users',
+        number: '32643',
+    },
+    {
+        icons:<FiUsers />,
+        name: 'InActive Users',
+        number: '34523',
+    },
+    {
+        icons:<FaUserSecret />,
+        name: 'Employees',
+        number: '32367',
+    },
+    {
+        icons:<FaUserSlash />,
+        name: 'Total Amount',
+        number: '323834',
+    }
+]
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { DashboardData } from '../Data/DashboardData'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='bg-[#252a48] h-screen'>
+        <div className='p-[20px]'>
+            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:grid-cols-3 gap-4'>
+                {
+                    DashboardData.map((item) => {
+                        return (
+                            <div className='bg-[#2c3157] rounded-2xl px-3 py-5 w-full'>
+                                <div className='flex justify-between'>
+                                    <div className='bg-[#9d3efa] text-white p-3 rounded'>{item.icons}</div>
+                                    <div>
+                                        <div className='text-gray-500 text-[13px]'>{item.name}</div>
+                                        <div className='text-white font-bold'>{item.number}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    </div>
   )
 }
