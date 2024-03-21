51eb821f41474e5a6faf8f29a595b90107ffbb50
angeiradu <a.iradukund2@alustudent.com>
2024-03-21 02:42:10 -0700

    The cleaner community App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route element={<PageLayout />}>
+    </Routes>
+  </BrowserRouter> 
+  );
+}
+
+export default App;
